c74535c131c92a5f74affeee81502d108c387ced
mytoh <mytoh@gmail.com>
2013-05-26 07:08:59 +0900

    * plugins/talikko/init.fish: add talikko plugin

--- plugins/lehti/init.fish
+++ plugins/lehti/init.fish
@@ -1,12 +1,14 @@
 
 # lehti {{{
+
+# mosh
 if test $MOSH_LOADPATH
   set -x MOSH_LOADPATH (lehti setup load-path fish) $MOSH_LOADPATH
 else
   set -x MOSH_LOADPATH (lehti setup load-path fish)
 end
 
-
+# ypsilon
 if test $YPSILON_SITELIB
   set -x YPSILON_SITELIB (lehti setup load-path):$YPSILON_SITELIB
 else
--- plugins/loitsu/init.fish
+++ plugins/loitsu/init.fish
@@ -1,8 +1,7 @@
-
 set loitsu $HOME/.loitsu
 set loitsu_lib $loitsu/lib
 set loitsu_bin $loitsu/bin
-
+ 
 set -x PATH $loitsu_bin $PATH
 
 # mosh
@@ -10,15 +9,15 @@ if test $MOSH_LOADPATH
 set -x MOSH_LOADPATH $loitsu_lib $MOSH_LOADPATH
 else
 set -x MOSH_LOADPATH $loitsu_lib
-end
-
+ end
+ 
 # ypsilon
  if test $YPSILON_SITELIB
 set -x YPSILON_SITELIB $loitsu_lib $YPSILON_SITELIB
  else
   set -x YPSILON_SITELIB $loitsu_lib
  end
-
+ 
 if test $YPSILON_LOADPATH
 set -x YPSILON_LOADPATH $loitsu_bin $YPSILON_LOADPATH
 else
