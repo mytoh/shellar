67c428064ecafb63bd24f03f27e04abeb5f75eaa
mytoh <mytoh@gmail.com>
2013-06-17 19:45:33 +0900

    * plugins/pikkukivi/init.fish: add pikkukivi plugin for fish

---
+++ plugins/pikkukivi/init.fish
@@ -0,0 +1,20 @@
+
+
+# pikkukivi {{{
+
+alias paths 'piki print-path'
+alias aa    'piki ascii-taide'
+alias futaba 'piki futaba'
+alias yotsuba  'piki yotsuba'
+
+alias mkd 'piki mkd'
+alias starwars 'piki starwars'
+alias jblive 'piki jblive'
+alias sumo 'piki sumo'
+alias sumo2 'piki sumo2'
+alias sumo3 'piki sumo3'
+alias gsp 'piki gsp'
+
+
+# }}}
+
