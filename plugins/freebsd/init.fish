2016069e03988142d7ff9be3590e5645a7c6af14
mytoh <mytoh@gmail.com>
2013-07-21 00:19:07 +0900

    * plugins/freebsd/init.fish: add http_proxy

--- plugins/freebsd/init.fish
+++ plugins/freebsd/init.fish
@@ -5,6 +5,8 @@ case FreeBSD
   set -x SDL_VIDEODRIVER vgl
   set -x XDG_DATA_DIRS /usr/local/kde4/share
 
+  set -x http_proxy 127.0.0.1:3128
+
   #if test {$TERM} = "cons25"
   #if test -e (which jfbterm)
   #  jfbterm
