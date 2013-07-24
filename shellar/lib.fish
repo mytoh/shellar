8f69a4ec288c427d0c17f7583f7a5375cc2ae1f2
mytoh <mytoh@gmail.com>
2013-07-24 23:55:23 +0900

    * shellar/lib.fish: add some functions

--- shellar/lib.fish
+++ shellar/lib.fish
@@ -10,3 +10,47 @@ set -gx shellar_custom_plugin_dir {$shellar_custom}/plugins
 
 # path
 set -gx PATH {$shellar_bin_dir} {$PATH}
+
+function shellar_register_paths
+  # [[http://github.com/mashiro/dot-files/blob/master/.zshenv]]
+  set dir {$argv[1]}
+  if test -d {$dir}
+
+    if test -d {$dir}/bin
+      if not contains {$dir}/bin {$PATH}
+        set -gx PATH {$dir}/bin {$PATH}
+      end
+    end
+    if test -d {$dir}/sbin
+      if not contains {$dir}/sbin {$PATH}
+        set -gx PATH {$dir}/sbin {$PATH}
+      end
+    end
+    if test -d {$dir}/man
+      if not contains {$dir}/man {$MANPATH}
+        set -gx MANPATH {$dir}/man {$MANPATH}
+      end
+    end
+    if test -d {$dir}/share/man
+      if not contains {$dir}/share/man {$MANPATH}
+        set -gx MANPATH {$dir}/share/man {$MANPATH}
+      end
+    end
+    if test -d {$dir}/info
+      if not contains {$dir}/info {$INFOPATH}
+        set -gx INFOPATH {$dir}/info {$INFOPATH}
+      end
+    end
+
+  end
+end
+
+function shellar_push_to_path
+  for p in $argv
+    if test -d $p
+      if not contains $p $PATH
+        set -gx PATH $p $PATH
+      end
+    end
+  end
+end
